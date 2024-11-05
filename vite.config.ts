ff69e06ad749ca66c98d45110fec7859fc7282de
M.Bálint <157951130+molnarb1408@users.noreply.github.com>
2024-11-05 20:53:58 +0100

    up

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,36 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
   build: {
     outDir: 'build',
-    emptyOutDir: true 
+    rollupOptions: {
+      output: {
+        entryFileNames: 'static/js/[name].js',
+        chunkFileNames: 'static/js/[name].js',
+        assetFileNames: (assetInfo) => {
+          const exts = assetInfo.names ? assetInfo.names.map(name => path.extname(name)) : [];
+          if (exts.length === 0) return 'static/[name][extname]';
+
+          for (const ext of exts) {
+            if (/\.css$/.test(ext)) {
+              return 'static/css/[name][extname]';
+            }
+            if (/\.png$|\.jpg$|\.jpeg$|\.gif$|\.svg$/.test(ext)) {
+              return 'static/media/images/[name][extname]';
+            }
+            if (/\.ttf$|\.otf$|\.woff$|\.woff2$/.test(ext)) {
+              return 'static/media/fonts/[name][extname]';
+            }
+            if (/\.pdf$/.test(ext)) {
+              return 'static/media/pdfs/[name][extname]';
+            }
+          }
+          return 'static/[name][extname]';
+        }
+      }
+    }
   }
-})
+});
