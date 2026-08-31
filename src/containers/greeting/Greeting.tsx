f05e2aab8e43987eed4e0ef05431723c0a362b11
M.Bálint <157951130+molnarb1408@users.noreply.github.com>
2026-08-31 15:15:01 +0200

    en fix

--- src/containers/greeting/Greeting.tsx
+++ src/containers/greeting/Greeting.tsx
@@ -8,7 +8,7 @@ import Button from "../../components/button/Button.tsx";
 import { illustration, greeting } from "../../portfolio.tsx";
 import { useStyle } from "../../contexts/StyleContext.tsx";
 import CV_hu from './CV_Molnar_Balint_HU.pdf';
-
+import CV_en from './CV_Molnar_Balint_EN.pdf';
 
 export default function Greeting() {
   const { isDark } = useStyle();
@@ -49,7 +49,13 @@ export default function Greeting() {
                     isDownload
                     downloadFile="CV_Molnar_Balint_HU.pdf" className={undefined} newTab={undefined} />
                 )}
-
+                {greeting.resumeLink && (
+                  <Button
+                    text="Önéletrajz EN"
+                    href={CV_en}
+                    isDownload
+                    downloadFile="CV_Molnar_Balint_EN.pdf" className={undefined} newTab={undefined} />
+                )}
               </div>
             </div>
           </div>
